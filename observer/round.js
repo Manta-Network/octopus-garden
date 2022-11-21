c8eebb9781e0148883080cfa54843e2e0913d989
Rob Thijssen <rthijssen@gmail.com>
2022-11-21 06:53:48 +0200

    round

--- observer/round.js
+++ observer/round.js
@@ -15,6 +15,7 @@ const uri = {
   cert: `${process.env.HOME}/.mongodb/X509-cert-7650484215012813007.pem`,
 };
 const client = new MongoClient(`${uri.scheme}://${uri.host}/${uri.database}?authMechanism=${uri.auth.mechanism}&authSource=${encodeURIComponent(uri.auth.source)}&tls=${uri.tls}&tlsCertificateKeyFile=${encodeURIComponent(uri.cert)}`);
+//const client = new MongoClient(`mongodb://localhost:27017`);
 const range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
 const randomInt = (min, max) => {
   min = Math.ceil(min);
@@ -22,69 +23,75 @@ const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-//const provider = new WsProvider('wss://ws.archive.calamari.systems');
-const provider = new WsProvider(`wss://a${randomInt(1, 4)}.calamari.systems`);
+const provider = new WsProvider('wss://ws.archive.calamari.systems');
+//const provider = new WsProvider(`wss://a${randomInt(1, 5)}.calamari.systems`);
 
 
 (async () => {
   const api = await ApiPromise.create({ provider });
   const collection = client.db(uri.database).collection(uri.collection);
-  while(true) {
+  for (let o = 0; o < Infinity; o++) {
     try {
+      const observedRounds = ((await collection.find({}, { projection: { _id: false, number: true } }).toArray()) || []).map(r=>r.number);
       const [
         round,
         lastHeader,
-        candidatePool,
       ] = await Promise.all([
         api.query.parachainStaking.round(),
         api.rpc.chain.getHeader(),
-        api.query.parachainStaking.candidatePool(),
       ]);
-      const sessionKeys = await Promise.all(candidatePool.map(cp => api.query.session.nextKeys(cp.owner)));
-      const collators = candidatePool.map((c, cI) => {
-        return {
-          account: c.owner,
-          ...JSON.parse(JSON.stringify(sessionKeys[cI])),
-        }
-      });
       const [ firstBlockInCurrentRound, roundLength, currentRound ] = [ 'first', 'length', 'current' ].map((key) => parseInt(round[key], 10));
       const lastEver = parseInt(lastHeader.number, 10);
       const rounds = range(1, currentRound).reverse();
-      for (let roundNumber = currentRound - 1; roundNumber > 0; roundNumber--) {
-        const firstNumber = (firstBlockInCurrentRound - ((currentRound - roundNumber) * roundLength));
-        const firstHash = await api.rpc.chain.getBlockHash(firstNumber);
-        const firstBlock = await api.rpc.chain.getBlock(firstHash);
-        const records = await (await api.at(firstHash)).query.system.events();
-        const first = {
-          number: firstNumber,
-          hash: firstHash,
-          events: firstBlock.block.extrinsics.map(({ method: { method, section } }, index) => ({
-            method: `${section}.${method}`,
-            events: records.filter(({ phase }) => phase.isApplyExtrinsic && phase.asApplyExtrinsic.eq(index)).map(({ event }) => event),
+      for (let number = currentRound; number > 0; number--) {
+        if (observedRounds.includes(number)) {
+          console.log(`iteration ${o}: skipping previously observed round ${number}`);
+          continue;
+        }
+        const first = (firstBlockInCurrentRound - ((currentRound - number) * roundLength));
+        const roundStartHash = await api.rpc.chain.getBlockHash(first);
+        const apiAtRoundStart = await api.at(roundStartHash);
+        const apiAtRoundEnd = (number === currentRound)
+          ? api
+          : await api.at(await api.rpc.chain.getBlockHash(first + roundLength));
+        const collators = await apiAtRoundStart.query.parachainStaking.candidatePool();
+        const [
+          topDelegations,
+          awardedPts,
+          nextKeys,
+        ] = await Promise.all([
+          apiAtRoundStart.query.parachainStaking.topDelegations.multi(collators.map(c => c.owner)),
+          apiAtRoundEnd.query.parachainStaking.awardedPts.multi(collators.map(c => [number, c.owner])),
+          apiAtRoundStart.query.session.nextKeys.multi(collators.map(c => c.owner)),
+        ]);
+        const top = JSON.parse(JSON.stringify(topDelegations));
+        const points = JSON.parse(JSON.stringify(awardedPts));
+        const round = {
+          number: number,
+          first,
+          stakeholders: JSON.parse(JSON.stringify(collators)).map((collator, i) => ({
+            collator: {
+              ...collator,
+              nimbus: JSON.parse(JSON.stringify(nextKeys[i])).nimbus,
+              points: points[i],
+            },
+            nominators: top[i].delegations,
           })),
         };
-        const lastNumber = (firstNumber + roundLength - 1);
-        const lastHash = await api.rpc.chain.getBlockHash(lastNumber);
-        const last = {
-          number: lastNumber,
-          hash: lastHash,
-        };
-
-
-        const dbRound = {
-          number: roundNumber,
-          blocks: {
-            first,
-            last,
-          },
-         };
-         console.log(JSON.stringify(dbRound));
-
-        //const update = await collection.updateOne({ number: roundNumber }, { $set:  }, { upsert: true });
+        const update = await collection.updateOne({ number: round.number }, { $set: round }, { upsert: true });
+        console.log({
+          number: round.number,
+          collators: round.stakeholders.length,
+          authors: round.stakeholders.map(sh=>sh.collator.nimbus).length,
+          nominators: round.stakeholders.reduce((a, c) => (a + c.nominators.length), 0),
+          stake: round.stakeholders.reduce((aC, c) => (aC + BigInt(c.nominators.reduce((aN, n) => (aN + BigInt(n.amount)), BigInt(0)))), BigInt(0)),
+          update,
+        });
       }
-      //await new Promise(r => setTimeout(r, 1000));
     } catch (error) {
       console.error(error);
     }
+    console.log(`iteration ${o}: sleeping for a minute...`);
+    await new Promise(r => setTimeout(r, 60000));
   }
 })()
