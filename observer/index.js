168edf991178f355b1cef46d230501de2dea0144
Rob Thijssen <rthijssen@gmail.com>
2023-01-09 18:02:36 +0200

    one log line

--- observer/index.js
+++ observer/index.js
@@ -17,46 +17,6 @@ const uri = {
 };
 const client = new MongoClient(`${uri.scheme}://${uri.host}/${uri.database}?authMechanism=${uri.auth.mechanism}&authSource=${encodeURIComponent(uri.auth.source)}&tls=${uri.tls}&tlsCertificateKeyFile=${encodeURIComponent(uri.cert)}`);
 
-async function syncAll() {
-  try {
-    const database = client.db(uri.database);
-    const collection = database.collection(uri.collection);
-    const api = await ApiPromise.create({ provider: wsProvider });
-    await api.isReady;
-    const [
-      round,
-      lastHeader,
-    ] = await Promise.all([
-      api.query.parachainStaking.round(),
-      api.rpc.chain.getHeader(),
-    ]);
-    const [ firstInCurrentRound, roundLength, currentRound ] = [ 'first', 'length', 'current' ].map((key) => parseInt(round[key], 10));
-    const lastEver = parseInt(lastHeader.number, 10);
-    for (let round = currentRound; round > 0; round--) {
-      const firstInThisRound = (round === currentRound)
-        ? firstInCurrentRound
-        : firstInCurrentRound - (roundLength * (currentRound - round));
-      const lastInThisRound = (round === currentRound)
-        ? lastEver
-        : firstInThisRound + roundLength - 1;
-      for (let number = lastInThisRound; number >= firstInThisRound; number--) {
-        const hash = await api.rpc.chain.getBlockHash(number);
-        const header = await api.derive.chain.getHeader(hash);
-        const block = {
-          round,
-          number,
-          hash: hash.toString(),
-          author: JSON.parse(JSON.stringify(header.digest.logs))[0].preRuntime[1],
-        };
-        const update = await collection.updateOne({ number }, { $set: block }, { upsert: true });
-        console.log(`round: ${round}, block: ${block.number}, author: ${block.author}, db: ${(!!update.modifiedCount) ? 'updated' : (!!update.upsertedCount) ? 'inserted' : 'observed'}`);
-      }
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 async function syncRound() {
   try {
     const database = client.db(uri.database);
@@ -82,7 +42,7 @@ async function syncRound() {
         author: JSON.parse(JSON.stringify(header.digest.logs))[0].preRuntime[1],
       };
       const update = await collection.updateOne({ number }, { $set: block }, { upsert: true });
-      console.log(`round: ${round}, block: ${block.number}, author: ${block.author}, db: ${(!!update.modifiedCount) ? 'updated' : (!!update.upsertedCount) ? 'inserted' : 'observed'}`);
+      console.log(`round: ${currentRound}, block: ${block.number}, author: ${block.author}, db: ${(!!update.modifiedCount) ? 'updated' : (!!update.upsertedCount) ? 'inserted' : 'observed'}`);
       if (update.upsertedId === null) {
         return await new Promise(r => setTimeout(r, 5000));
       }
